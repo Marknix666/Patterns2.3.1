761ef5d3702815a91cd018e7883af1b9cbc211be
Marknix <116298439+Marknix666@users.noreply.github.com>
2023-07-09 22:09:22 +0300

    good com

--- src/test/java/ru/netology/card/test/CardDiliveyTest.java
+++ src/test/java/ru/netology/card/test/CardDiliveyTest.java
@@ -45,7 +45,7 @@ public class CardDiliveyTest {
                 .shouldBe(visible);
         $("[data-test-id='replan-notification'] button").click();
         $("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на" + SecondsMeetingDate))
+                .shouldHave(exactText("Встреча успешно запланирована на " + SecondsMeetingDate))
                 .shouldBe(visible);
     }
 }
